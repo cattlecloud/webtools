89c57eda45e5155957d1aaf1aa23915ca2c58137
Seth Hoenig <shoenig@duck.com>
2026-06-05 08:30:50 -0500

    middles: support for cache control layer

---
+++ middles/cache.go
@@ -0,0 +1,36 @@
+package middles
+
+import (
+	"net/http"
+	"strings"
+	"time"
+
+	"cattlecloud.net/go/webtools"
+	"cattlecloud.net/go/webtools/runtime"
+	"github.com/shoenig/lang"
+)
+
+type Cache struct {
+	Env runtime.Environment
+	H   http.Handler
+}
+
+func (c *Cache) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	var ttl time.Duration
+
+	switch c.Env.String() {
+	case runtime.Local:
+		ttl = 5 * time.Second
+
+	case runtime.Staging:
+		css := strings.HasSuffix(r.URL.Path, ".css")
+		ttl = lang.Maybe(css, 1*time.Minute, 1*time.Hour)
+
+	case runtime.Production:
+		css := strings.HasSuffix(r.URL.Path, ".css")
+		ttl = lang.Maybe(css, 3*time.Hour, 24*time.Hour)
+	}
+
+	webtools.SetCacheControl(w, ttl)
+	c.H.ServeHTTP(w, r)
+}
